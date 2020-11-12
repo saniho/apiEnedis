c96a59b7dd90fe1971278b2253e1c372baa9b2bc
saniho <52693545+saniho@users.noreply.github.com>
2020-11-12 20:40:50 +0100

    première version recupération des données de la veille, mois, année courante et année precedente

---
+++ sensor.py
@@ -0,0 +1,112 @@
+"""Sensor for my first"""
+import logging
+from collections import defaultdict
+from datetime import timedelta
+
+import voluptuous as vol
+
+import homeassistant.helpers.config_validation as cv
+from homeassistant.components.sensor import PLATFORM_SCHEMA
+from homeassistant.const import (
+    CONF_NAME,
+    CONF_TOKEN,
+    CONF_CODE,
+    CONF_SCAN_INTERVAL,
+    ATTR_ATTRIBUTION,
+)
+
+from homeassistant.helpers.entity import Entity
+from homeassistant.util import Throttle
+from homeassistant.util import slugify
+from homeassistant.util.dt import now, parse_date
+
+_LOGGER = logging.getLogger(__name__)
+
+DOMAIN = "saniho"
+
+ICON = "mdi:package-variant-closed"
+
+SCAN_INTERVAL = timedelta(seconds=1800)
+
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
+    {
+        vol.Required(CONF_TOKEN): cv.string,
+        vol.Optional(CONF_CODE): cv.string,
+        vol.Optional(CONF_NAME): cv.string,
+    }
+)
+
+from . import apiEnedis
+
+
+def setup_platform(hass, config, add_entities, discovery_info=None):
+    """Set up the platform."""
+    _LOGGER.warning("passe ici - 1")
+    name = config.get(CONF_NAME)
+    token = config.get(CONF_TOKEN)
+    code = config.get(CONF_CODE)
+    update_interval = config.get(CONF_SCAN_INTERVAL, SCAN_INTERVAL)
+
+    try:
+        session = []
+    except :
+        _LOGGER.exception("Could not run my First Extension")
+        return False
+    _LOGGER.warning("passe ici %s %s " %( token, code ))
+    myDataEnedis = apiEnedis.apiEnedis( token, code )
+    add_entities([myEnedis(session, name, update_interval, myDataEnedis )], True)
+    # on va gerer  un element par heure ... maintenant
+
+class myEnedis(Entity):
+    """."""
+
+    def __init__(self, session, name, interval, myDataEnedis):
+        """Initialize the sensor."""
+        self._session = session
+        self._name = name
+        self._myDataEnedis = myDataEnedis
+        self._attributes = None
+        self._state = None
+        self.update = Throttle(interval)(self._update)
+
+    @property
+    def name(self):
+        """Return the name of the sensor."""
+        return "myEnedis"
+
+    @property
+    def state(self):
+        """Return the state of the sensor."""
+        return self._state
+
+    @property
+    def unit_of_measurement(self):
+        """Return the unit of measurement of this entity, if any."""
+        return ""
+
+    def _update(self):
+        """Update device state."""
+        import datetime
+        status_counts = defaultdict(int)
+
+        _LOGGER.warning("call update")
+        status_counts["lastSynchro"] = datetime.datetime.now()
+        status_counts['yesterday'] = self._myDataEnedis.getYesterday()
+        status_counts['last_month'] = self._myDataEnedis.getLastMonth()
+        status_counts['current_month'] = self._myDataEnedis.getCurrentMonth()
+        status_counts['last_year'] = self._myDataEnedis.getLastYear()
+        status_counts['current_year'] = self._myDataEnedis.getCurrentYear()
+        #status_counts['yesterday'] = ""
+        self._attributes = {ATTR_ATTRIBUTION: ""}
+        self._attributes.update(status_counts)
+        ## pour debloquer
+        self._state = " "
+
+    @property
+    def device_state_attributes(self):
+        """Return the state attributes."""
+        return self._attributes
+
+    @property
+    def icon(self):
+        """Icon to use in the frontend."""
