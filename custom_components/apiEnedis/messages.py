e38a388c0549dc6f5bfcc11aea8ac633d2c70068
saniho <52693545+saniho@users.noreply.github.com>
2020-12-26 23:08:58 +0100

    message erreur en francais config flow

---
+++ custom_components/apiEnedis/messages.py
@@ -0,0 +1,17 @@
+listeMessages = \
+{
+    "ERR_001" : "",
+    "token_refresh_401" : "Erreur de token",
+    "no_data_found" : "Donnees non disponible",
+    "client_not_found": "Client inconnu",
+    "Invalid_request": "Erreur requete",
+    "Internal Server error": "Erreur Interne Enedis",
+    "result_500": "Erreur interne Enedis(500)",
+    "UNKERROR_001": "Erreur interne Enedis(001)",
+}
+
+def getMessage( codeMessage ):
+    if ( codeMessage in listeMessages.keys()):
+        return listeMessages[ codeMessage ]
+    else:
+        return codeMessage
