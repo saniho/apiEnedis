65483df72e5dc1f5cc8b17e20cf470904110163f
saniho <52693545+saniho@users.noreply.github.com>
2021-01-08 12:38:06 +0100

    add sensor yesterdaycost few pytest manage more enedis error

--- custom_components/apiEnedis/const.py
+++ custom_components/apiEnedis/const.py
@@ -24,5 +24,5 @@ DEFAULT_SCAN_INTERVAL = 60*5  # verification enedis toutes les 5 minutes, si der
 DEFAULT_SENSOR_INTERVAL = 60 # 60 secondes verifications du coordinator
 
 
-__VERSION__ = "1.1.1.0"
+__VERSION__ = "1.1.1.1"
 __name__ = "myEnedis"
