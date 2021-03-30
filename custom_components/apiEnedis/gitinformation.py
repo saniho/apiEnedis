28ed42ea74a8e76b423154789d60c7b3281f3927
saniho <52693545+saniho@users.noreply.github.com>
2021-03-30 21:41:24 +0200

    ajout wait 1 secondes avant appel api gestion de l'erreur ADAM-ERR0075( collecte horaire non activée ) getGitVersion

---
+++ custom_components/apiEnedis/gitinformation.py
@@ -0,0 +1,20 @@
+import logging
+
+import urllib, json
+
+_LOGGER = logging.getLogger(__name__)
+
+class gitinformation:
+    def __init__(self, repo):
+        self._serverName = "https://api.github.com/repos/%s/releases/latest" %(repo)
+        self._gitData = None
+
+    def getInformation(self):
+        from urllib.request import urlopen
+        myURL = urlopen(self._serverName)
+        s = myURL.read()
+        dataAnswer = json.loads(s)
+        self._gitData = dataAnswer
+
+    def getVersion(self):
+        return self._gitData.get("tag_name", "")
