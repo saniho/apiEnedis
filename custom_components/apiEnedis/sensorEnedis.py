3f6577b0fe7b7ad2abc6e125674f44792a818903
mdeweerd <mdeweerd@users.noreply.github.com>
2022-03-20 23:14:16 +0100

    Apply fix for packaging.version

--- custom_components/apiEnedis/sensorEnedis.py
+++ custom_components/apiEnedis/sensorEnedis.py
@@ -106,6 +106,7 @@ class manageSensorState:
 
     def getExistsRecentVersion(self, versionCurrent, versionGit):
         import packaging
+        import packaging.version
 
         # If only one of the versions exists, result is False
         if (versionCurrent is None) or (versionGit is None):
